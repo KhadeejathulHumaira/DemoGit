ab6c01fe91d9c6045ea1d7f1f859afe481e1697a
Khadeejathul Humaira <humairazulbiha@gmail.com>
2021-08-05 13:41:24 +0530

    added

--- Demo.js
+++ Demo.js
@@ -2,3 +2,11 @@ console.log("This is demo")
 console.log("this is a conflict")
 const h1="happy"
 console.log("This is For trail ")
+console.log("This is demo")
+console.log("this is a conflict")
+const h1="happy"
+console.log("This is For trail ")
+const animal="Wolf"
+const bird="crow"
+const h1="happy"
+console.log("This is For trail ")
